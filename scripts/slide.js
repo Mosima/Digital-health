6fa8335ac321999de37e67454ee1578581e832c1
cebo shezi <cebo@kohde.io>
2017-08-25 14:42:16 +0200

    edited

--- scripts/slide.js
+++ scripts/slide.js
@@ -11,7 +11,7 @@
             slideIndex++;
             if (slideIndex > x.length) {slideIndex = 1} 
             x[slideIndex-1].style.display = "block"; 
-            setTimeout(carousel, 10000); // Change image every 2 seconds
+            setTimeout(carousel, 10000); // Change image every 2 second
         }
 
     
